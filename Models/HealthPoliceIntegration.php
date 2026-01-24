36a9cabca353fd37fd00cc63c59c2f59e32ce6df
dowyx <dricxterrosano0@gmail.com>
2026-01-24 04:17:57 -0800

    Merge remote and local repositories

--- Models/HealthPoliceIntegration.php
+++ Models/HealthPoliceIntegration.php
@@ -1,6 +1,8 @@
 <?php
 // Start session and check authentication
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 if (!isset($_SESSION['user_id'])) {
     header('Location: ../login.php');
     exit();
