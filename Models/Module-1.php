a302be44dbee3764b140a5a09c01c8f9d52e35f6
dowyx <dricxterrosano0gmail.com>
2026-02-25 09:35:22 +0800

    Add server setup and configuration files

--- Models/Module-1.php
+++ Models/Module-1.php
@@ -290,61 +290,61 @@ function get_campaign_icon($type) {
                 <h1>Public Safety</h1>
             </div>
             <ul class="nav-menu">
-                <li class="nav-item">
-                    <a href="../home.php" class="nav-link">
-                        <i class="fas fa-home"></i>
-                        <span class="nav-text">Dashboard</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="module-1.php" class="nav-link active">
-                        <i class="fas fa-calendar-alt"></i>
-                        <span class="nav-text">Campaign Planning & Calendar</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="content-repository.php" class="nav-link">
-                        <i class="fas fa-database"></i>
-                        <span class="nav-text">Content Repository</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="target-group-segmentation.php" class="nav-link">
-                        <i class="fas fa-users"></i>
-                        <span class="nav-text">Target Group Segmentation</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="eventseminarmanagement.php" class="nav-link">
-                        <i class="fas fa-calendar-check"></i>
-                        <span class="nav-text">Event & Seminar Management</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="surveyfeedbackcollection.php" class="nav-link">
-                        <i class="fas fa-clipboard-check"></i>
-                        <span class="nav-text">Survey & Feedback Collection</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="campaignanalyticsreports.php" class="nav-link">
-                        <i class="fas fa-chart-bar"></i>
-                        <span class="nav-text">Campaign Analytics & Reports</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="healthpoliceintegration.php" class="nav-link">
-                        <i class="fas fa-link"></i>
-                        <span class="nav-text">Community</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="../logout.php" class="nav-link">
-                        <i class="fas fa-sign-out-alt"></i>
-                        <span class="nav-text">Logout</span>
-                    </a>
-                </li>
-            </ul>
+    <li class="nav-item">
+        <a href="../home.php" class="nav-link">
+            <i class="fas fa-home"></i>
+            <span class="nav-text">Dashboard</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="Module-1.php" class="nav-link">
+            <i class="fas fa-calendar-alt"></i>
+            <span class="nav-text">Campaign Planning & Calendar</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="ContentRepository.php" class="nav-link">
+            <i class="fas fa-database"></i>
+            <span class="nav-text">Content Repository</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="TargetGroupSegmentation.php" class="nav-link">
+            <i class="fas fa-users"></i>
+            <span class="nav-text">Target Group Segmentation</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="EventSeminarManagement.php" class="nav-link">
+            <i class="fas fa-calendar-check"></i>
+            <span class="nav-text">Event & Seminar Management</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="SurveyFeedbackCollection.php" class="nav-link">
+            <i class="fas fa-clipboard-check"></i>
+            <span class="nav-text">Survey & Feedback Collection</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="CampaignAnalyticsReports.php" class="nav-link active">
+            <i class="fas fa-chart-bar"></i>
+            <span class="nav-text">Campaign Analytics & Reports</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="HealthPoliceIntegration.php" class="nav-link">
+            <i class="fas fa-link"></i>
+            <span class="nav-text">Community</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="../logout.php" class="nav-link">
+            <i class="fas fa-sign-out-alt"></i>
+            <span class="nav-text">Logout</span>
+        </a>
+    </li>
+</ul>
         </aside>
 
         <!-- Main Content -->
